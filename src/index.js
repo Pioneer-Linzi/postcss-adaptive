6a5e666940277c9d3d4389a885869a45bd6fa0fb
linzi <linzi4235@gmail.com>
2018-06-22 14:58:09 +0800

    add exclude file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import Adaptive from './adaptive'
 
 export default postcss.plugin(pkg.name, (options) => {
   return (css, result) => {
+    if (options.exclude && css.source.input.file.match(options.exclude) !== null) {
+      result.root = css;
+      return
+    }
     const adaptiveIns = new Adaptive(options)
     const output = adaptiveIns.parse(css.toString())
     result.root = postcss.parse(output)
